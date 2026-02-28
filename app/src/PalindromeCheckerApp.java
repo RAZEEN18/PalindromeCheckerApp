c129be80f293ee330cb54c980fe0be5e0b9f294e
RAZEEN18 <razeenmujeeb143@gmail.com>
2026-02-28 12:14:39 +0530

    uc4-charactercheck

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,23 +1,21 @@
 import java.util.Scanner;
 
 public class PalindromeCheckerApp{
-
-    public static void main(String[] args){
-                Scanner sc = new Scanner(System.in);
-                System.out.print("Enter text: ");
-                String input = sc.nextLine();
-                String reversed = "";
-                for (int i = input.length() - 1; i >= 0; i--) {
-                    reversed = reversed + input.charAt(i);
+            public static void main(String[] args) {
+                        String input = "radar";
+                        char[] chars = input.toCharArray();
+                        int start = 0;
+                        int end = chars.length - 1;
+                        boolean isPalindrome = true;
+                        while (start < end) {
+                            if (chars[start] != chars[end]) {
+                                isPalindrome = false;
+                                break;
+                            }
+                            start++;
+                            end--;
+                        }
+                        System.out.println("Input : " + input);
+                        System.out.println("Is Palindrome? : " + isPalindrome);
+                    }
                 }
-                boolean isPalindrome = input.equals(reversed);
-                System.out.println("Reversed text: " + reversed);
-                System.out.println("Is it a Palindrome? : " + isPalindrome);
-                sc.close();
-            }
-        }
-
-
-
-
-
