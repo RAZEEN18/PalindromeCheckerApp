136525267f616cb25f456cc7269d25937a2183ac
RAZEEN18 <razeenmujeeb143@gmail.com>
2026-03-09 14:02:56 +0530

    uc6-queue and stack

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,20 +1,30 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class PalindromeCheckerApp{
-            public static void main(String[] args) {
-                        String input = "noon";
-                        Stack<Character> stack = new Stack<>();
-                        for (char c : input.toCharArray()) {
-                            stack.push(c);
-                        }
-                        boolean isPalindrome = true;
-                        for (char c : input.toCharArray()) {
-                            if (c != stack.pop()) {
-                                isPalindrome = false;
-                                break;
-                            }
-                        }
-                        System.out.println("Input : " + input);
-                        System.out.println("Is Palindrome? : " + isPalindrome);
-                    }
-                }
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+        String input = "civic";
+        Queue<Character> queue = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : input.toCharArray()) {
+            queue.add(c);
+            stack.push(c);
+        }
+
+        boolean isPalindrome = true;
+
+        while (!queue.isEmpty()) {
+            char qChar = queue.remove();
+            char sChar = stack.pop();
+
+            if (qChar != sChar) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
